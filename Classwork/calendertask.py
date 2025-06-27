e5caecab5efe6e3d9e84326d443dcf43f0b3b3c6
Haarsh <hp962682@gmail.com>
2025-06-27 20:53:08 +0530

    op

--- Classwork/calendertask.py
+++ Classwork/calendertask.py
@@ -1,4 +1,17 @@
 
-a = int(input("enter the number= "))
-b = int(input("enter the value=  "))
+# PRINT THE USING TWO VARIAVLE TO USING OPERATOR .
 
+a = int(input("enter the year= "))
+b = int(input("enter the months=  "))
+c =input("enter the operator +,-,*,/ :")
+
+if c =='+':
+    print("The result is :",a+b)
+elif c =='-':
+    print("The result is :",a-b)
+elif c =='*':
+    print("The result is :",a*b)
+elif c =='/':
+    print("The result is :",a/b)
+else:
+    pass
