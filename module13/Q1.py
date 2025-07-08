7981d6e7f03a040d97aac8a377d586ddfd67a8be
Haarsh <hp962682@gmail.com>
2025-07-08 15:09:35 +0530

    new

---
+++ module13/Q1.py
@@ -0,0 +1,7 @@
+# Write a Python program that prints "Hello, World!"
+
+print("Hello,world!")
+
+# Set up Python on your local machine and write a program to display your name.
+
+print("My name is Harsh Patel")
---
+++ module13/Q11.py
@@ -0,0 +1,9 @@
+
+#Write a Python program to find the length of each string in List1.
+
+
+List1 = ['apple', 'banana', 'mango']
+
+for fruit in List1:
+    length = len(fruit)
+    print(f"The length of '{fruit}' is {length}.")
