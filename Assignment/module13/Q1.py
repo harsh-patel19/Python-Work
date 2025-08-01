9aec1aa4b80e1d02df8323964f7c7694d7d95e0f
Haarsh <hp962682@gmail.com>
2025-08-01 09:33:36 +0530

    hello

--- Assignment/module13/Q1.py
+++
@@ -1,7 +0,0 @@
-# Write a Python program that prints "Hello, World!"
-
-print("Hello,world!")
-
-# Set up Python on your local machine and write a program to display your name.
-
-print("My name is Harsh Patel")
